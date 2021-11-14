3aa886f128df98921df197b7b67d94d347b07b08
saoja <54879266+saoja@users.noreply.github.com>
2021-11-14 15:16:16 -0600

    Update Driver.cpp
    added team class and updated plater class

--- Driver.cpp
+++ Driver.cpp
@@ -1,3 +1,5 @@
+
+
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -7,45 +9,108 @@ using namespace std;
 class Player{
     public:
     string name;
+    string team;
     int age;
     int recivingYards;
     int recivingTargets;
-    int roiutsRun;
+    int routesRun;
 
-    //calculations
+    //calculations vars
     int yprr = 0;
     int ypt =0;
+    
+    string getName(){
+        return name;
+    }
+    string getTeam(){
+        return team;
+    }
+    int getAge(){
+        return age;
+    }
+    int getrecvYards(){ return recivingYards;}
+    int getrecvTargets(){ return recivingTargets;}
+    int getroutesRun(){ return routesRun;}
+    
+    
+    
+    void setName(string name){
+        this->name=name;
+    }
+     void setTeam(string team){
+        this->team=team;
+    }
+    void setAge(int age){
+        this->age=age;
+    }
+    void setRecvingYards(int recivingYards){this->recivingYards=recivingYards;}
+    void setrecivingTargets(int recivingTargets){this->recivingTargets=recivingTargets;}
+    void setroutesRun(int routesRun){this->routesRun=routesRun;}
+    
+    //calculations vars
+    //Yards per routes run
+    int yprr (){
+        yppr=recivingYards/routesRun;
+        return yppr;
+    }
+    //yards per Target
+    int ypt(){
+        ypt =  recivingYards/ recivingTargets;
+    return ypt;
+}
+    
 
 
 
 };
-//basic Functions for calclations. Number of Calculations not final 
-//Yards per routes run
-int yprr (int recvYards,int routesRun){
-    return recvYards/routesRun;
-}
-//yards per Target
-int ypt(int recvYards, int recvTargets){
-    return recvYards/ recvTargets;
-}
+class Team {
+    public:
+    vector<Player> members;
+    string name;
+    int totalWins;
+    double winProb;
+    
+    void addPlayer(Player p){
+        members.push_back(p);
+    }
+    
+    //calc
+    double calcWinProb(Team A, Team B){
+        return 0;
+    }
+    
+    
+};
+
+
+
+
 int main(){
-    int N = 1000;// size of list
+    int N = 1000;// size of lines in file 
     vector<Player> list;
+    vector<Team> teams;
 
     //input data from file
     for(int i = 0; i<N;i++){
         Player temp;
-        temp.name= "";//all stats would be from file these are place holders
-        temp.age= 25;
-        temp.recivingYards= 100;
-        temp.recivingTargets = 25;
-        temp.roiutsRun = 10;
-        //calculations
-        temp.yprr=yprr(temp.recivingYards,temp.roiutsRun);
-        temp.ypt=ypt(temp.recivingYards,temp.recivingTargets);
+       temp.setName(" ");
+       temp.setTeam(" ");
+       temp.setAge(25);
+       temp.setrecivingTargets(10);
+       temp.setRecvingYards(100);
+       temp.setroutesRun(5);
+       
+       // calc 
+       temp.yppr();
+       temp.ypt();
+        
+       list.push_back(temp);
 
-        list.push_back(temp);
-
+    }
+    for(int i =0; i< 33 ; i++){
+        Team temp; 
+        for (int i = 0 ; i<list.size();  )
+        
     }
     
        
